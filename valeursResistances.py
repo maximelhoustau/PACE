94529b13c91e915fe48056ac60a272f10db85e06
Etienne BURLE <etienne.burle@telecom-paristech.fr>
2018-06-21 16:30:15 +0200

    programmes d'optimisation du nombre de résistances

--- valeursResistances.py
+++ valeursResistances.py
@@ -6,7 +6,7 @@ Created on Thu Jun 21 09:59:14 2018
 @author: eburle
 """
 
-L1 = [1.1,8.3,380,67.7,5.8,380,680,120,22,27,270,100,150,7.6,4.8,680,180,220,33,467,148,2.3,473000,470000,21800,8200,75000,180000,68000,564000,218000,81300,39500,6700,56000,33100,4700,75000,9900000,5580000,3300000,1500000,2000000]
+L1 = [1.1,8.3,67.7,5.8,390,680,120,22,27,270,100,150,7.6,4.8,680,180,220,33,467,148,2.3,473000,470000,21800,8200,75000,180000,68000,564000,218000,81300,39500,6700,56000,33100,4700,75000,9900000,5580000,3300000,1500000,2000000]
 
 L1 = sorted(L1)
 
@@ -45,8 +45,25 @@ L = L10 + L2 + L3
 L = sorted(L)
 N = len(L)
 
+def rechercher1(r):
+    a = 0
+    b = n - 1
+    while (b - a > 1):
+        c = (a+b)//2
+        if (L1[c] < r):
+            a = c
+        elif (L1[c] >r):
+            b = c
+        else:
+            a = c
+            b = c
+    return L1[a]
 
-def rechercher(r):
+def trouverPoid1(w, Rf):
+    l = rechercher1(Rf/w)
+    return l, (Rf/l)
+
+def rechercher3(r):
     a = 0
     b = N - 1
     while (b - a > 1):
@@ -60,7 +77,94 @@ def rechercher(r):
             b = c
     return L[a]
 
-Req = 270
 
-def trouverPoid(w):
-    return rechercher(Req/w)
+def trouverPoid3(w, Rf):
+    l = rechercher3(Rf/w)
+    return l, (Rf/l[0])
+
+n2 = len(L2)
+
+def rechercher2(r):
+    a = 0
+    b = n2 - 1
+    while (b - a > 1):
+        c = (a+b)//2
+        if (L2[c][0] < r):
+            a = c
+        elif (L2[c][0] >r):
+            b = c
+        else:
+            a = c
+            b = c
+    return L2[a]
+
+
+def trouverPoid2(w, Rf):
+    l = rechercher2(Rf/w)
+    return l, (Rf/l[0])
+
+def trouverRf2(liste):
+    R = 0
+    d = 1000
+    for r in L1:
+        d1 = 0
+        for w in liste:
+            d1 += (w-trouverPoid2(w,r)[1])**2
+        if (d1 < d):
+            d = d1
+            R = r
+    return R, d
+
+def trouverRf3(liste):
+    R = 0
+    d = 1000
+    for r in L1:
+        d1 = 0
+        for w in liste:
+            d1 += (w-trouverPoid3(w,r)[1])**2
+        if (d1 < d):
+            d = d1
+            R = r
+    return R, d
+
+dmax = 0.05
+
+def trouverRes(liste,Rf):
+    Liste = []
+    m = 0
+    dt = 0
+    for w in liste:
+        v = trouverPoid1(w, Rf)
+        d = abs(w-v[1])
+        if (d<dmax):
+            Liste.append(v)
+            m += 1
+            dt += d
+        else:
+            v = trouverPoid2(w, Rf)
+            d = abs(w-v[1])
+            if (d<dmax):
+                Liste.append(v)
+                m += 2
+                dt += d
+            else:
+                v = trouverPoid3(w, Rf)
+                Liste.append(v)
+                m += 3
+                dt += abs(w-v[1])
+    return Liste, m, dt
+    
+
+def toutTrouver(liste,dtmax):
+    Rf = 0
+    m = 100
+    L = []
+    for R in L1:
+        res = trouverRes(liste,R)
+        if ((res[2]<dtmax) and (res[1]<m)):
+            m = res[1]
+            Rf = R
+            L = res
+    return Rf, m, L[0]
+        
+        
